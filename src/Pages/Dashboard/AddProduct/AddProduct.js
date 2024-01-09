2ffbd4345e4fffac80835f6db9d8aeac743ae148
AlAminMiah4274 <alaminmiah4274@gmail.com>
2024-01-09 20:50:34 +0600

    created AddProduct component

--- src/Pages/Dashboard/AddProduct/AddProduct.js
+++ src/Pages/Dashboard/AddProduct/AddProduct.js
@@ -1,9 +1,101 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const AddProduct = () => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const handleAddProductForm = data => {
+
+        console.log(data);
+    };
+
     return (
-        <div>
-            <h1 className="text-3xl">Add A Product</h1>
+        <div className="border lg:w-3/5 mx-auto p-6 my-10">
+            <h1 className="text-3xl text-center mb-5">Add A Product</h1>
+
+            <form onSubmit={handleSubmit(handleAddProductForm)}>
+
+                <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+                    <label className="form-control w-full">
+                        <div className="label"><span className="label-text">Product Name</span></div>
+                        <input type="text" {...register("productName", {
+                            required: "product name is required"
+                        })} placeholder="Name" className="input input-bordered w-full" />
+                    </label>
+
+                    <label className="form-control w-full">
+                        <div className="label"><span className="label-text">Product Price</span></div>
+                        <input type="text" {...register("productPrice", {
+                            required: "product price is required"
+                        })} placeholder="Price" className="input input-bordered w-full" />
+                    </label>
+
+                    <label className="form-control w-full">
+                        <div className="label"><span className="label-text">Product Condition</span></div>
+                        <select {...register("productCondition")} className="select select-bordered">
+                            <option value='excellent'>Excellent</option>
+                            <option value='good'>Good</option>
+                            <option value='fair'>Fair</option>
+                        </select>
+                    </label>
+
+                    <label className="form-control w-full">
+                        <div className="label"><span className="label-text">Seller Phone Number</span></div>
+                        <input type="text" {...register("sellerNumber", {
+                            required: "phone number is required"
+                        })} placeholder="Number" className="input input-bordered w-full" />
+                    </label>
+
+                    <label className="form-control w-full">
+                        <div className="label"><span className="label-text">Seller Location</span></div>
+                        <select {...register("sellerLocation")} className="select select-bordered">
+                            <option value='dhaka'>Dhaka</option>
+                            <option value='barishal'>Barishal</option>
+                            <option value='chittagong'>Chittagong</option>
+                            <option value='khulna'>Khulna</option>
+                            <option value='sylhet'>Sylhet</option>
+                            <option value='cumilla'>Cumilla</option>
+                            <option value='mymensingh'>Mymensingh</option>
+                            <option value='rangpur'>Rangpur</option>
+                        </select>
+                    </label>
+
+                    <label className="form-control w-full">
+                        <div className="label"><span className="label-text">Product Category</span></div>
+                        <select {...register("productCategory")} className="select select-bordered">
+                            <option value='computer science'>Computer Science</option>
+                            <option value='history'>History</option>
+                            <option value='fiction'>Fiction</option>
+                            <option value='literature'>Literature</option>
+                            <option value='self development'>Self Development</option>
+                            <option value='politics'>Politics</option>
+                        </select>
+                    </label>
+
+                    <label className="form-control w-full">
+                        <div className="label"><span className="label-text">Year of Purchase</span></div>
+                        <input type="text" {...register("purchaseYear", {
+                            required: "purchase is required"
+                        })} placeholder="Year" className="input input-bordered w-full" />
+                    </label>
+                    <label className="form-control w-full">
+                        <div className="label"><span className="label-text">Product Image</span></div>
+                        <input type="file" {...register("productImage")} className="file-input file-input-bordered w-full" />
+                    </label>
+                </div>
+
+                <label className="form-control w-full">
+                    <div className="label"><span className="label-text">Description</span></div>
+                    <textarea {...register("description", {
+                        required: "description is required"
+                    })} className="textarea textarea-bordered h-24" placeholder="Write your thoughts"></textarea>
+                </label>
+
+                <input type="submit" value="Submit" className="btn btn-success w-full mt-10 text-white" />
+
+            </form>
+
         </div>
     );
 };
