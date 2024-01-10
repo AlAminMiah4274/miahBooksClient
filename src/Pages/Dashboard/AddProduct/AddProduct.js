68a642fc4a77c299ad809940aab07f1ddc24651c
AlAminMiah4274 <alaminmiah4274@gmail.com>
2024-01-10 22:04:45 +0600

    able to upload image in image hosting site in AddProduct

--- src/Pages/Dashboard/AddProduct/AddProduct.js
+++ src/Pages/Dashboard/AddProduct/AddProduct.js
@@ -1,13 +1,34 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const AddProduct = () => {
 
     const { register, handleSubmit, formState: { errors } } = useForm();
+    const imageHostKey = process.env.REACT_APP_imghostkey;
+    const navigate = useNavigate();
 
     const handleAddProductForm = data => {
 
-        console.log(data);
+        const image = data.productImage[0];
+        const form = new FormData();
+        form.append("image", image);
+
+        fetch(`https://api.imgbb.com/1/upload?key=${imageHostKey}`, {
+            method: "POST",
+            body: form
+        })
+            .then(res => res.json())
+            .then(imgData => {
+
+                if(imgData.success){
+                    toast.success("your image successfully uploaded");
+                    navigate("/dashboard/myProduct");
+                };
+            })
+
+        console.log(image);
     };
 
     return (
@@ -22,6 +43,8 @@ const AddProduct = () => {
                         <input type="text" {...register("productName", {
                             required: "product name is required"
                         })} placeholder="Name" className="input input-bordered w-full" />
+
+                        {errors?.productName && <p className="text-red-500 text-sm">{errors?.productName?.message}</p>}
                     </label>
 
                     <label className="form-control w-full">
@@ -29,6 +52,8 @@ const AddProduct = () => {
                         <input type="text" {...register("productPrice", {
                             required: "product price is required"
                         })} placeholder="Price" className="input input-bordered w-full" />
+
+                        {errors?.productPrice && <p className="text-red-500 text-sm">{errors?.productPrice?.message}</p>}
                     </label>
 
                     <label className="form-control w-full">
@@ -45,6 +70,8 @@ const AddProduct = () => {
                         <input type="text" {...register("sellerNumber", {
                             required: "phone number is required"
                         })} placeholder="Number" className="input input-bordered w-full" />
+
+                        {errors?.sellerNumber && <p className="text-red-500 text-sm">{errors?.sellerNumber?.message}</p>}
                     </label>
 
                     <label className="form-control w-full">
@@ -78,10 +105,16 @@ const AddProduct = () => {
                         <input type="text" {...register("purchaseYear", {
                             required: "purchase is required"
                         })} placeholder="Year" className="input input-bordered w-full" />
+
+                        {errors?.purchaseYear && <p className="text-red-500 text-sm">{errors?.purchaseYear?.message}</p>}
                     </label>
                     <label className="form-control w-full">
                         <div className="label"><span className="label-text">Product Image</span></div>
-                        <input type="file" {...register("productImage")} className="file-input file-input-bordered w-full" />
+                        <input type="file" {...register("productImage", {
+                            required: "product image is required"
+                        })} className="file-input file-input-bordered w-full" />
+
+                        {errors?.productImage && <p className="text-red-500 text-sm">{errors?.productImage?.message}</p>}
                     </label>
                 </div>
 
@@ -90,6 +123,8 @@ const AddProduct = () => {
                     <textarea {...register("description", {
                         required: "description is required"
                     })} className="textarea textarea-bordered h-24" placeholder="Write your thoughts"></textarea>
+
+                    {errors?.description && <p className="text-red-500 text-sm">{errors?.description?.message}</p>}
                 </label>
 
                 <input type="submit" value="Submit" className="btn btn-success w-full mt-10 text-white" />
